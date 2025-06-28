0e2bcb587ed1f5e0df3d8bf5ea557445e1f2fe40
Mielgo98 <pablomm529@gmail.com>
2025-06-28 10:11:41 +0200

    cambios en la configuracion

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // 🔧 ESTO ES CLAVE para que las rutas funcionen desde el sistema de archivos o subcarpetas
   plugins: [react()],
   build: {
     outDir: 'dist',
